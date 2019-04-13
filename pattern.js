26f529c0f72035ab0edadcb9ae510e7b9d0c06d8
Jesus Arevalo <isa90201@gmail.com>
2019-04-13 12:27:45 -0700

    Clkeaned up code and removed some output code.

--- pattern.js
+++ pattern.js
@@ -12,9 +12,6 @@ function AreEqual(a, b) {
 
 //-------------------------
 
-let charTypes = ['a', 'n', 's'];
-let patternTypes = ['alpha', 'numeric', 'alphanumeric'];
-
 class CharInfoFactory {
   constructor() {
   }
@@ -52,16 +49,10 @@ function GetPatternType(str) {
 
   let patternType = null;
 
-  if (symbolCount == 0) {
-    if (alphaCount == charInfoArr.length)
-      patternType = 'alpha';
-    else if (numericCount == charInfoArr.length)
-      patternType = 'numeric';
-    else
-      patternType = 'alphanumeric';
-  }
+  if (numericCount == charInfoArr.length)
+    patternType = 'number';
   else
-    patternType = 'symbols';
+    patternType = 'string';
 
   return patternType;
 }
@@ -147,6 +138,76 @@ function NumberPatternToFormatString(pattern) {
   return formatStr;
 }
 
+function HasNumericPart(strInfo) {
+  let parts = strInfo.patternParts;
+
+  for (let i = 0; i < parts.length; ++i) {
+    let currPart = parts[i];
+
+    if (currPart.patternType == 'number')
+      return true;
+  }
+
+  return false;
+}
+
+function AllPartsMatchExceptForOne(strInfo1, strInfo2) {
+  let parts1 = strInfo1.patternParts;
+  let parts2 = strInfo2.patternParts;
+
+  if (parts1.length != parts2.length)
+    return false;
+
+  let mismatchHappened = false;
+
+  for (let i = 0; i < parts1.length; ++i) {
+    let currPart1 = parts1[i];
+    let currPart2 = parts2[i];
+
+    if (currPart1.patternType == currPart2.patternType) {
+      let value1 = currPart1.value;
+      let value2 = currPart2.value;
+
+      if (value1 != value2) {
+        if (mismatchHappened)
+          return false;
+
+        mismatchHappened = true;
+      }
+    }
+    else
+      return false;
+  }
+
+  return true;
+}
+
+function AllPartsMatchForAtLeastOneOtherInfo(strInfo, otherInfosArr) {
+  let boolArr = otherInfosArr.map(x => AllPartsMatchExceptForOne(strInfo, x));
+  let trueItems = boolArr.filter(x => x == true);
+  return trueItems.length > 0;
+}
+
+function GetOtherInfosWhereAllPartsMatchExceptForOne(strInfo, otherInfosArr) {
+  let potentialMatches = otherInfosArr.filter(x => AllPartsMatchExceptForOne(strInfo, x) == true);
+  return potentialMatches;
+}
+
+function GetMismatchIndex(strInfo1, strInfo2) {
+  let parts1 = strInfo1.patternParts;
+  let parts2 = strInfo2.patternParts;
+
+  for (let i = 0; i < parts1.length; ++i) {
+    let currPart1 = parts1[i].value;
+    let currPart2 = parts2[i].value;
+
+    if (currPart1 != currPart2)
+      return i;
+  }
+
+  return null;
+}
+
 
 function GetConsolidatedList(strArr) {
   if (strArr.length == 1)
@@ -156,14 +217,87 @@ function GetConsolidatedList(strArr) {
   let strInfoFactory = new StringInfoFactory();
   let infos = strArr.map(x => strInfoFactory.GetStringInfo(x));
 
-  infos.forEach(x => {
-    console.log(`\n\nCHAR_INFO_ARRAY: ${JSON.stringify(x.charInfoArr)}`);
-    console.log(`PATTERN_STRING: ${x.patternStr}`);
-    console.log(`PATTERN_PARTS: ${JSON.stringify(x.patternParts)}`);
+  // Conslidate filenames
+
+  let doneChecking = [];  // Place info objects that have been checked here
+  let hasBeenConsolidated = [];
+  let consolidatedNames = [];
+
+  console.log('\nConsolidating filenames...'); // DEBUG
+
+  for (let i = 0; i < infos.length; ++i) {
+    let currInfo = infos[i];
+    console.log(`\n  CURR_INFO (i = ${i}): ${currInfo.patternParts.map(x => x.value).join('')}`); // DEBUG
+
+    if (!hasBeenConsolidated.includes(currInfo)) {
+      let otherInfos = infos.filter(x => x != currInfo).filter(x => !doneChecking.includes(x)).filter(x => !hasBeenConsolidated.includes(x));
+      let potentialMatches = GetOtherInfosWhereAllPartsMatchExceptForOne(currInfo, otherInfos);
+
+      if (potentialMatches.length != 0) {
+        console.log(`  POTENTIAL_MATCHES (${potentialMatches.length}): ${potentialMatches.map(x => x.patternParts.map(y => y.value).join(''))}`); // DEBUG
+
+        let consolidationCount = 0;
+
+        for (let k = 0; k < potentialMatches.length; ++k) {
+          let currRemaining = potentialMatches[k];
+          console.log(`  REMAINING_INFO (k = ${k}): ${currRemaining.patternParts.map(x => x.value).join('')}`); // DEBUG
+
+          if (currInfo.patternStr == currRemaining.patternStr && AllPartsMatchExceptForOne(currInfo, currRemaining)) {
+            let index = GetMismatchIndex(currInfo, currRemaining);
+            console.log(`  MISMATCH_INDEX = ${index}`); // DEBUG
+
+            let replacementStr = '*';
+            let mismatchInfo = currRemaining.patternParts[index];
+
+            if (mismatchInfo.patternType == 'number') {
+              let numFormatStr = NumberPatternToFormatString(mismatchInfo.patternStr);
+              replacementStr = numFormatStr;
+            }
+
+            let strArr = currRemaining.patternParts.map(x => x.value);
+            strArr[index] = replacementStr;
+
+            consolidationCount += 1; // DEBUG
+            let consolidatedStr = strArr.join('');
+
+            if (!consolidatedNames.includes(consolidatedStr))
+              consolidatedNames.push(consolidatedStr);
+
+            if (!hasBeenConsolidated.includes(currRemaining))
+              hasBeenConsolidated.push(currRemaining);
+
+            doneChecking.push(currRemaining);
+          }
+        }
+
+        if (consolidationCount == 0) {
+          if (!hasBeenConsolidated.includes(currInfo))
+            hasBeenConsolidated.push(currInfo);
+
+          let name = currInfo.patternParts.map(x => x.value).join('');
+          if (!consolidatedNames.includes(name))
+            consolidatedNames.push(name);
+        }
+
+        doneChecking.push(currInfo);
+      }
+      else {
+        console.log('  Info is unique.')
+        doneChecking.push(currInfo);
+        hasBeenConsolidated.push(currInfo);
+
+        let name = currInfo.patternParts.map(x => x.value).join('');
+        consolidatedNames.push(name);
+      }
+    }
+  }
+
+  console.log(`\nCONSOLIDATED_NAMES:`);
+  consolidatedNames.forEach(str => {
+    console.log(`  ${str}`);
   });
 
-  // CONT 999
-  // Conslidate filenames
+  return consolidatedNames;
 }
 
 //--------------------------------------
@@ -173,8 +307,12 @@ let strArr = [
   'isa_1.png',
   'isa_2.png',
   'isa_arevalo.txt',
+  'isa_zzzzzzz.txt',
+  'isa_zuzuarregui.txt',
   'a.x',
-  'b.x'
+  'b.x',
+  '123.x',
+  '001.x'
 ];
 
-GetConsolidatedList(strArr);
+let names = GetConsolidatedList(strArr);
